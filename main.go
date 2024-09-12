56ff40898484c8601018eddb6affc69f22045605
Jirayu Aunkhonthee <126430616+chotchoterd@users.noreply.github.com>
2024-09-12 11:31:50 +0700

    commits

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"rsc.io/quote/v3"
-)
+import "github.com/chotchoterd/gobasic/variables"
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println(quote.GlassV3())
+	// fmt.Println("Hello World!")
+	// fmt.Println(quote.GlassV3())
+	variables.Learn()
 }
